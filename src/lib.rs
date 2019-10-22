3d5ccad229598aef958757ebc120c9a8b2f12195
João Henrique Oliveira <joaojhro@gmail.com>
2019-10-22 11:03:34 +0200

    final project

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,14 +16,26 @@ impl Config {
         let query = args[1].clone(); //* Big runtime cost, but we accept for now
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -35,6 +49,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     for line in contents.lines() {
         //* returns an iterator the lines() method
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -66,8 +93,9 @@ mod test {
 Rust:
 safe, fast, productive.
 Pick three.
-Trust me.";
-        assert_eq!(vec!["safe", "fast", "productive"], search(query, contents));
+Trust me.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
